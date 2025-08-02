5c63bd49cbf89fe43237be5d81383eb6ee881c95
PiyushKingg <piyushthedev69@gmail.com>
2025-08-02 09:00:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'dashboard')));
+
+module.exports = app;
